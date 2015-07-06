08ee2ea29e30cbbd29ad130e457cf26ade6f8c18
DoyuLy <duyud@qq.com>
2015-07-07 01:37:12 +0800

    those are two test files

---
+++ test.js
@@ -0,0 +1,16 @@
+//把form表单传递进去
+function validateForm(form, options){
+	//遍历form中的表单标签
+	for (var obj in form) {
+		switch case 遍历类型
+		接着写各自的空值判断 和  正则
+
+		最后抛出异常提示 return msg()
+	};
+
+	通过则提交  没通过则阻止
+}
+
+function msg(type ,options){
+
+}
