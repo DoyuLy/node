c987f8a92548cfa6e0c33a1024047ad2af42aa0a
DoyuLy <duyud@qq.com>
2015-08-16 19:00:59 +0800

    test

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-//把form表单传递进去
-function validateForm(form, options){
-	//遍历form中的表单标签
-	for (var obj in form) {
-		switch case 遍历类型
-		接着写各自的空值判断 和  正则
+var events = require('events');
+//var emitter = new events.EventEmitter();
 
-		最后抛出异常提示 return msg()
-	};
-
-	通过则提交  没通过则阻止
+function myObj(){
+    events.EventEmitter.call(this);
 }
+myObj.prototype.__proto__ = events.EventEmitter.prototype;
 
-function msg(type ,options){
-
-}
