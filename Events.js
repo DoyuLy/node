c2234d1d3c1fa0130be2b0f21219e4834680ce03
DoyuLy <duyud@qq.com>
2015-08-22 23:54:54 +0800

    add files

--- Events.js
+++ Events.js
@@ -1,3 +1,13 @@
+/*
+    addListener(eventName, cb)
+    on(eventName, cb)
+    once(eventName, cb)
+
+    .listeners(eventName) 返回监听事件的监听器数组
+    .setMaxListeners(n)   设置监听器最大数,默认是10(大于10发出警报)
+    .removeListener(eventName, cb) 移出cb
+ */
+
 var events = require('events');
 //var emitter = new events.EventEmitter();
 
--- fs/fs.js
+++ fs/fs.js
@@ -10,3 +10,82 @@
  *  }
  *  ps: 异步自动处理异常 (err, msg) /  同步必须手动try... catch...
  */
+
+var util = require('util');
+var fs = require('fs');
+var async = require('async');
+var _ = require('underscore');
+
+//----------------------同步打开文件----------------
+//var fd = fs.openSync("F:\\git\\node\\composer.json", 'w');
+//console.log(fd);
+//fs.closeSync(fd);
+
+//----------------------异步打开文件----------------
+//fs.open("F:\\git\\node\\composer.json", 'w', function (err, fd) {
+//    console.log(fd);
+//    if(!err)
+//        fs.close(fd);
+//})
+
+//----------------------写入文件----------------
+/**
+ * 可以写入buffer 或 string
+ * options : encoding/mode/flag
+ *           flag: r/r+/rs/rs+ :读(不存在则异常) / 读写(不存在则异常) / 同步模式读(绕过缓存,且不同于openSync,避免使用) / 同步模式读写(同rs)
+ *                 w/wx/w+/wx+ :写(不存在则创建,有则截断) / 写(存在则打开失败,同w相反) / 读写(不存在则创建,有则截断) / 读写(存在则打开失败,同w+相反)
+ *                 a/ax/a+/ax+ :追加(不存在则追加) / 追加(存在则打开失败) / 读取,追加(不存在则追加) / 读取,追加(存在则打开失败)
+ *           mode: 访问模式 默认0666(可读可写)
+ * fs.writeFile(path, data, [options], cb) //不需要返回文件描述符来手动关闭文件
+ * fs.writeFileSync(path, data, [options]) //它返回了一个文件描述符 fd
+ * fs.write(fd, data, offset, length, position, cb)
+ *    params{
+ *      fd    :fs.open / fs.openSync 返回的文件描述符
+ *      data  :string / buffer
+ *      offset:写入数据的索引
+ *      length:写入的长度
+ *      position:在文件写入的位置索引
+  *     cb: 回调参数为(err, bytes) bytes为写入字节数
+ *    }
+ */
+
+//var config = {
+//    maxFiles: 20,
+//    maxConnections: 10,
+//    rootPath: '/webroot'
+//};
+//
+//var options = {encoding: 'utf8', flag: 'w'};
+//
+//fs.writeFile('./test.txt', JSON.stringify(config), options, function (err) {
+//    if(err) console.log('config write failed.');
+//    else console.log('config saved.')
+//});
+
+var friut = ['apple', 'orange', 'banana', 'grapes'];
+async.waterfall([
+    function(cb){
+        fs.open('./test.txt', 'w', '0666', function (err, fd) {
+            err ? cb(err) : cb(null, fd);
+        })
+    },
+    function(fd, cb){
+        if(fd){
+            _.each(friut, function(val, i){
+                (function(val){
+                    fs.write(fd, val+ ' '+i, null, null, function(err, bytes){
+                        console.log(i); //0 3 2 1
+                        err ? cb(err) : cb(null, val, bytes);
+                    })
+                })(val)
+            });
+        }else{
+            cb('open file error.')
+        }
+    }
+],function(err, val, bytes){
+    //只会拿到最后一个回调
+    err ? console.log(err) : console.log("write : %s %d bytes", val, bytes);
+})
+
+
